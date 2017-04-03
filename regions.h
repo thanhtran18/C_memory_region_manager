694171096e002171c66aaf811743c804c5d9dc43
thanhtran18 <tranct@myumanitoba.ca>
2017-04-03 17:06:37 -0500

    Downloaded the question and given .c, .h files

---
+++ regions.h
@@ -0,0 +1,17 @@
+#ifndef _REGIONS_H
+#define _REGIONS_H
+
+typedef enum { false, true } Boolean;
+
+typedef unsigned short r_size_t;
+
+Boolean rinit(const char *region_name, r_size_t region_size);
+Boolean rchoose(const char *region_name);
+const char *rchosen();
+void *ralloc(r_size_t block_size);
+r_size_t rsize(void *block_ptr);
+Boolean rfree(void *block_ptr);
+void rdestroy(const char *region_name);
+void rdump();
+
+#endif
